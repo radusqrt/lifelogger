2fa59063695e0c76a186894da1fc751bc9cea81e
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2022-02-13 00:14:59 +0100

    Add detailed/event view. Use templates for index and event views. Finished part 3.

--- events/views.py
+++ events/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Event
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the events index.")
+    latest_events_list = Event.objects.order_by('-creation_time')[:5]
+    context = {
+        'latest_events_list': latest_events_list
+    }
+    return render(request, 'events/index.html', context)
+
+
+def detail(request, event_id):
+    event = get_object_or_404(Event, id=event_id)
+    return render(request, "events/detail.html", {'event': event})
