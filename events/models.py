ef39aff6e30ebe2bf679dfed860d0663a36348f5
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2022-02-12 23:39:35 +0100

    Create Event model. Add constraints for Event type. Register Event app in admin site.

--- events/models.py
+++ events/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class EventType(models.TextChoices):
+    SPORT = "SPORT"
+    SYMPTOM = "SYMPTOM"
+
+
+class Event(models.Model):
+    creation_time = models.DateTimeField()
+    target_time = models.DateTimeField(null=True, blank=True, default=None)
+    type = models.CharField(choices=EventType.choices, max_length=20)
+
+    def __str__(self) -> str:
+        return "Creation time: {0}, Target time: {1}, Type: {2}".format(self.creation_time, self.target_time, self.type)
+
+    def was_created_recently(self) -> bool:
+        return self.creation_time >= timezone.now() - datetime.timedelta(days=1)
+
+    class Meta:
+        constraints = [models.CheckConstraint(
+            name="$(app_label)s_%(class)s_type_valid", check=models.Q(type__in=EventType.values))]
