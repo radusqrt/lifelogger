05a83469170bc1dcd7922dd25b1b34a4d83018b8
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2022-02-17 17:37:37 +0100

    Remove secret key from settings

--- lifelogger/settings.py
+++ lifelogger/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "cq%1*@62hgg9q10nvwn-e44oosyqnx(5*e=6)vrtny0k&5n=)%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
